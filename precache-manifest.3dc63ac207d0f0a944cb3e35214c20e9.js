dc5e9db807be14c141b097795814d6ea9dd10ed7
노다설 <sia0940@gmail.com>
2020-12-17 16:33:59 +0900

    Updates

---
+++ precache-manifest.3dc63ac207d0f0a944cb3e35214c20e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421d597b6562a467c4c4784b86c8f5df",
+    "url": "/lottolee/index.html"
+  },
+  {
+    "revision": "9afa2920395ac42b1341",
+    "url": "/lottolee/static/css/2.75056432.chunk.css"
+  },
+  {
+    "revision": "d70f435886bdac3b79cb",
+    "url": "/lottolee/static/css/main.0131ff6a.chunk.css"
+  },
+  {
+    "revision": "9afa2920395ac42b1341",
+    "url": "/lottolee/static/js/2.1a2a85ca.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/lottolee/static/js/2.1a2a85ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d70f435886bdac3b79cb",
+    "url": "/lottolee/static/js/main.207025e1.chunk.js"
+  },
+  {
+    "revision": "299d8bd5daad292971e4",
+    "url": "/lottolee/static/js/runtime-main.127d0a15.js"
+  },
+  {
+    "revision": "7975547fa4cb2a9d30f8d063d8d6eee6",
+    "url": "/lottolee/static/media/lotto.7975547f.png"
+  }
+]);
