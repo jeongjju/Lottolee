19b51c9674eb2150eb3b7d66a98d8512725bf342
jeongju.noh <jeongju.noh@hsdengine.com>
2021-01-13 09:01:53 +0900

    proxy를 사용하여 상대경로로 가져오도록 수정

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Homepage from './containers/homepage';
 class App extends React.Component{
     componentDidMount() {
-        fetch('http://localhost:3002/api')
+        fetch('/api')
           .then(res => res.json())
           .then(data => console.log('data',data));
       }
